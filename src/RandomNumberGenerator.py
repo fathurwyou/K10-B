a51302a2c0db9a31627680b86767f386e664bf62
Ellipsis <rizkyfathur326@gmail.com>
2024-04-27 11:20:10 +0700

    fix: delete testing code

--- src/RandomNumberGenerator.py
+++ src/RandomNumberGenerator.py
@@ -6,6 +6,3 @@ def random_number_generator(a: int, b: int, seed: int=42) -> int:
     random_number: int = a + seed % (b - a + 1)
 
     return random_number
-
-for i in range(1000,10000):
-    print(random_number_generator(1,100,i))
